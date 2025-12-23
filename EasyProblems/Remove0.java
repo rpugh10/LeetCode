9d9511e20963e8e54aa234cdd1e9e1ed75153262
Ryan Pugh <rpugh777@yahoo.com>
2025-12-22 23:09:06 -0800

    Completed the remove 0 problem

---
+++ EasyProblems/Remove0.java
@@ -0,0 +1,17 @@
+public class Remove0 {
+
+     public static void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                if(i != j){
+                    nums[i] = 0;
+                }
+                j++;
+            }
+            
+        }
+        
+    }
+}
